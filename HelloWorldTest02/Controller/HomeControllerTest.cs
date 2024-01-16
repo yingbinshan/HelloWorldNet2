4f09ac0a1f985061f39f25a391338fb68f081923
Eric Shan <2390540017@qq.com>
2024-01-16 17:17:36 +0800

    Add Unit Test

---
+++ HelloWorldTest02/Controller/HomeControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using HelloWorld.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace HelloWorldTest02.Controller
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        private HomeController _homeController;
+
+        [TestMethod]
+        public void Get_Home_Index_Should_Be_Null()
+        {
+            _homeController = new HomeController();
+            var result = (ViewResult)_homeController.Index();
+            result.Model.ShouldBeNull();
+        }
+    }
+}
---
+++ HelloWorldTest02/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloWorldTest02")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HelloWorldTest02")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9a8cfb8d-954b-4a4e-9497-1748dd803fee")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
